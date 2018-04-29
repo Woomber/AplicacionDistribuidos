7b246233a8a7035cb417f39a8e1c73041286adfd
Violinistae <ssbbemims@gmail.com>
2018-04-29 12:08:25 -0500

    Try Catch DB Servers
    Segundo Try Catch añadido para la conexión a la BD activa
    Faltan pruebas que hacer

--- database/DBConexion.php
+++ database/DBConexion.php
@@ -3,10 +3,19 @@
     Class DBConexion {
 
         private $host = "localhost";
+        //private $host = "IP Address DB Server #1";
         private $base = "coco";
         private $user = "root";
         private $pwd = "";
         private $charset = "utf8";
+
+        private $host2 = "localhost";
+        //private $host2 = "IP Address DB Server #2";
+        private $base2 = "coco";
+        private $user2 = "root";
+        private $pwd2 = "";
+        private $charset2 = "utf8";
+
         protected $pdo;
         public $status;
 
@@ -21,8 +30,20 @@
                 $this->status = 202;
                 return true;
             } catch (PDOException $ex) {
-                $this->status = 503;
-                return false;
+                try {
+                     $this->pdo = new PDO(
+                        'mysql:host='.$this->host2.';dbname='.$this->base2.';charset='.$this->charset2,
+                        $this->user2,
+                        $this->pwd2
+                    );
+                    $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                    $this->status = 203;
+                    return true;
+                } catch (PDOException $pdoex) {
+                    $this->pdo = null;
+                    $this->status = 503;
+                    return false;
+                }
             }
         }
 
